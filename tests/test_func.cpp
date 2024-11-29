06cd45c608b7f25c8ebc54ee196389e6f6611bac
miguelmendozajr <miguelmendozaj20@gmail.com>
2024-11-28 23:24:49 -0600

    add factorial

--- tests/test_func.cpp
+++ tests/test_func.cpp
@@ -1,13 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
-#include <tuple>
-#include <string>
 #include "func.h"
 
 
-TEST_CASE("contains code") {
-    REQUIRE(containsCode("abcabcabc", "abc") == 1);  // Match at index 1
-    REQUIRE(containsCode("aaaaa", "aaa") == 1);      // Overlapping match
-    REQUIRE(containsCode("abcdef", "xyz") == -1);    // No match
-    REQUIRE(containsCode("", "abc") == -1);          // Empty transmission
-    REQUIRE(containsCode("abc", "") == -1);          // Empty mcode
+TEST_CASE("factorial") {
+    REQUIRE(factorial(0) == 1);
 }
